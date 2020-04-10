3a106ee26420dc00514886da6d4d9e0a01367bb1
Gur Telem <gur111telem@gmail.com>
2020-04-10 04:01:12 -0700

    Fix broken code

--- Dry/q2.c
+++ Dry/q2.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *stringDuplicator(char *s, int times) {
-    assert(!s);
+char *stringDuplicator(const char *src, int times) {
+    assert(src);
     assert(times > 0);
-    int LEN = strlen(s);
-    char *out = malloc(LEN * times);
-    assert(out);
-    for (int i = 0; i < times; i++) {
-    out = out + LEN;
-    strcpy(out, s);
+    int len = strlen(src);
+    char *out = malloc(len * times + 1);
+
+    if (!out) {
+        return NULL;
     }
+
+    for (int i = 0; i < times; i++) {
+        strcat(out, src);
+    }
+
     return out;
 }
