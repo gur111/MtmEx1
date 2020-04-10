740ac1943b788d90a93afdd3dbc4a0c2a1a94973
Gur Telem <gur111telem@gmail.com>
2020-04-10 04:06:20 -0700

    Git some more stuff

--- Dry/q2.c
+++ Dry/q2.c
@@ -5,8 +5,8 @@
 char *stringDuplicator(const char *src, int times) {
     assert(src);
     assert(times > 0);
-    int len = strlen(src);
-    char *out = malloc(len * times + 1);
+    const int len = strlen(src);
+    char *const out = malloc(len * times + 1);
 
     if (!out) {
         return NULL;
