457b3141b390a78f476a198d4f0d1e85a9f45744
Gur Telem <gur111telem@gmail.com>
2020-04-11 17:49:11 -0700

    Add static function signature and template for mapGetNode

--- mtm_map/map.c
+++ mtm_map/map.c
@@ -16,6 +16,18 @@ struct Map_t {
     int count;
 };
 
+/**
+ *  mapGetNode: Returns the node associated with a specific key in the map(not a
+ * copy). *			Iterator status unchanged
+ * @param map - The map for which to get the node from.
+ * @param key - The key of the node we want to get.
+ * @return
+ *  NULL if a NULL pointer was sent or if the map does not contain the requested
+ *  key.
+ * 	Otherwise, pointer to the node associated with the given key.
+ */
+static Node mapGetNode(Map map, const char* key);
+
 Map mapCreate() {
     return NULL;  // TODO: Write functionality
 }
@@ -38,6 +50,10 @@ bool mapContains(Map map, const char* key) {
 
 MapResult mapPut(Map map, const char* key, const char* data) {
     return MAP_SUCCESS;  // TODO: Write functionality
+}
+
+static Node mapGetNode(Map map, const char* key) {
+    return NULL;  // TODO: Write functionality
 }
 
 char* mapGet(Map map, const char* key) {
