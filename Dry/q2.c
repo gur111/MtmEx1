634656954eff45c9bc1d66cf8b356836b050302d
Gur Telem <gur111telem@gmail.com>
2020-04-14 09:02:09 -0700

    Finish writing Question2

--- Dry/q2.c
+++ Dry/q2.c
@@ -2,7 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *stringDuplicator(const char *src, int times) {
+// Broken version:
+char *stringDuplicator(char *s, int times) {
+    assert(!s);
+    assert(times > 0);
+    int LEN = strlen(s);
+    char *out = malloc(LEN * times);
+    assert(out);
+    for (int i = 0; i < times; i++) {
+    out = out + LEN;
+    strcpy(out, s);
+    }
+    return out;
+}
+
+
+/* Everything that is wrong with it
+ * Conventions Mistakes:
+ * Function name isn't a verb
+ * Param "s" isn't descriptive
+ * "s" isn't const
+ * Local "LEN" should be lower cased
+ * "LEN" isn't const
+ * 
+ * Programmatic Errors:
+ * assert(!s) is checking the opposite of what we want (it's making sure the value IS NULL)
+ * malloc not allocating space for null terminator ('\0')
+ * malloc isn't verified (asserts are gone when not using a debug version)
+ * return is not returning the beggining of the string
+ */
+
+
+
+
+
+
+
+// Fixes version:
+char *stringDuplicate(const char *src, int times) {
     assert(src);
     assert(times > 0);
     const int len = strlen(src);
@@ -13,7 +50,7 @@ char *stringDuplicator(const char *src, int times) {
     }
 
     for (int i = 0; i < times; i++) {
-        strcat(out, src);
+        strcpy(out+sizeof(char)*i*len, src);
     }
 
     return out;
