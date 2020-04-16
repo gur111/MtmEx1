85dde745f2f242adcdafd809e4d1fc70f8d5a84b
Dani Bondar <danibondar@gmail.com>
2020-04-16 13:57:22 +0300

    added the election struct and electionCreat and electionAddTribe functions.

--- election/election.h
+++ election/election.h
@@ -7,7 +7,7 @@
 
 #include "../mtm_map/map.h"
 
-typedef struct election_t* Election;
+typedef struct election_t *Election;
 
 typedef enum ElectionResult_t {
     ELECTION_OUT_OF_MEMORY,
@@ -22,28 +22,28 @@ typedef enum ElectionResult_t {
     ELECTION_INVALID_VOTES
 } ElectionResult;
 
-typedef bool (*AreaConditionFunction) (int);
+typedef bool (*AreaConditionFunction)(int);
 
 Election electionCreate();
 
 void electionDestroy(Election election);
 
-ElectionResult electionAddTribe (Election election, int tribe_id, const char* tribe_name);
+ElectionResult electionAddTribe(Election election, int tribe_id, const char *tribe_name);
 
-ElectionResult electionAddArea(Election election, int area_id, const char* area_name);
+ElectionResult electionAddArea(Election election, int area_id, const char *area_name);
 
-const char* electionGetTribeName (Election election, int tribe_id);
+const char *electionGetTribeName(Election election, int tribe_id);
 
-ElectionResult electionAddVote (Election election, int area_id, int tribe_id, int num_of_votes);
+ElectionResult electionAddVote(Election election, int area_id, int tribe_id, int num_of_votes);
 
 ElectionResult electionRemoveVote(Election election, int area_id, int tribe_id, int num_of_votes);
 
-ElectionResult electionSetTribeName (Election election, int tribe_id, const char* tribe_name);
+ElectionResult electionSetTribeName(Election election, int tribe_id, const char *tribe_name);
 
-ElectionResult electionRemoveTribe (Election election, int tribe_id);
+ElectionResult electionRemoveTribe(Election election, int tribe_id);
 
 ElectionResult electionRemoveAreas(Election election, AreaConditionFunction should_delete_area);
 
-Map electionComputeAreasToTribesMapping (Election election);
+Map electionComputeAreasToTribesMapping(Election election);
 
 #endif //MTM_ELECTION_H
